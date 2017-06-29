2d8c584ce24caa9047371bb3b2cb5f3fc7dfe23c
SarathKumarCh <chidellasarathkumar@gmail.com>
2017-06-29 17:08:16 -0500

    Information about github paths URL's and other retreving from github

---
+++ AngularGithub_Route/public_html/github.js
@@ -0,0 +1,53 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function(){
+  
+  var github = function($http){
+    
+    var getUser = function(username){
+      return $http.get("https://api.github.com/users/" + username)
+              .then(function(response){
+                return response.data;
+              });
+    };
+    
+    var getRepos = function(user){
+      return $http.get(user.repos_url)
+              .then(function(response){
+                return response.data;
+              });
+    };
+    
+    var getRepoDetails = function(username, reponame){
+      var repo;
+      var repoUrl = "https://api.github.com/repos/" + username + "/" + reponame;
+      
+      return $http.get(repoUrl)
+        .then(function(response){
+          repo = response.data;
+          return $http.get(repoUrl + "/collaborators");
+        })
+        .then(function(response){
+          repo.collaborators = response.data;
+          return repo;
+        });
+    };
+    
+    return {
+      getUser: getUser,
+      getRepos: getRepos,
+      getRepoDetails: getRepoDetails
+    };
+    
+  };
+  
+  var module = angular.module("githubViewer");
+  module.factory("github", github);
+  
+  
+  
+})();
+
